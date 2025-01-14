3eaa86bde5a9af3d6c411c1df4af25b49b73f948
Aman-0707 <aman7kc@gmail.com>
2025-01-14 11:42:13 +0545

    com.example.nepal_app

--- lib/portfolioPage.dart
+++ lib/portfolioPage.dart
@@ -1,5 +1,7 @@
+import 'package:external_app_launcher/external_app_launcher.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PortfolioPage extends StatelessWidget {
   @override
@@ -35,7 +37,7 @@ class PortfolioPage extends StatelessWidget {
             Expanded(
               child: TabBarView(
                 children: [
-                  Container(),
+                  UrlLink(),
                   Container(),
                   AddSharesTab(),
                   Container(),
@@ -82,6 +84,72 @@ class AddSharesTab extends StatelessWidget {
             Container(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class UrlLink extends StatefulWidget {
+  const UrlLink({super.key});
+
+  @override
+  State<UrlLink> createState() => _UrlLinkState();
+}
+
+class _UrlLinkState extends State<UrlLink> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        // crossAxisAlignment: CrossAxisAlignment.start,
+        // ignore: prefer_const_literals_to_create_immutables
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // GestureDetector(
+          //   onTap: () async {
+          //     const url = 'https://www.google.com';
+          //     if (await canLaunch(url)) {
+          //       await launch(url, forceWebView: true, enableJavaScript: true);
+          //     } else {
+          //       throw 'Could not launch $url';
+          //     }
+          //   },
+          //   child: Center(
+          //     child: Text(
+          //       "Google",
+          //       style: TextStyle(
+          //           color: Colors.green,
+          //           fontSize: 25,
+          //           fontWeight: FontWeight.w400),
+          //     ),
+          //   ),
+          // ),
+          Center(
+            child: SizedBox(
+              height: 200,
+              width: 150,
+              child: Column(
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                      onPressed: () async {
+                        await LaunchApp.openApp(
+                            androidPackageName: 'com.example.nepal_app',
+                            openStore: false);
+                      },
+                      child: const Center(
+                        child: Text(
+                          "Open App",
+                          textAlign: TextAlign.center,
+                        ),
+                      )),
+                ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
